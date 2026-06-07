35c16a305a5c45d1ae9bef72e7ac039038404ac2
mayraisman <may@we-scale-up.com>
2026-06-07 13:41:43 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SCALEUP_CONFIG = {
+  webhookUrl: "https://script.google.com/macros/s/AKfycbwf5WAfDFHT0NlEkRKzRE6H1CofnsJDVehP2LegUmgzYVSCZWM15g4Wc5DELwKV4HRN/exec",
+  requestMode: "no-cors",
+  successMessage: "נרשמתם בהצלחה. פרטי ההתחברות יישלחו אליכם במייל.",
+  source: "scaleup-webinar-may-nir",
+};
